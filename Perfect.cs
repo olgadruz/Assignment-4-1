b25589baf384720167f771c99d3d152201abed61
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-28 23:39:43 -0400

    first draft

---
+++ Perfect.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Perfect;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int g = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i < g; i++)
+        {
+            if (g % i == 0)
+            {
+                sum = sum + i;
+            }
+        }
+        if (sum == g)
+        {
+            Console.WriteLine("Perfect Number");
+        }
+        else
+        {
+            Console.WriteLine("Not A Perfect Number");
+        }
+    }
+}
